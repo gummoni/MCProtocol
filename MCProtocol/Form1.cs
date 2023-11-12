c390b8008f68b16d5f69b8cab57450245905802c
koichi <shaty3@hotmail.com>
2023-11-13 00:49:41 +0900

    修正

--- MCProtocol/Form1.cs
+++ MCProtocol/Form1.cs
@@ -19,8 +19,15 @@ namespace MCProtocol
         public readonly Dictionary<int, bool> R = new();
         public readonly Dictionary<int, bool> MR = new();
         public readonly Dictionary<int, ushort> DM = new();
+        readonly Task DoTask;
+        public bool IsFinished => DoTask.IsCompleted;
 
-        public Task Start(int port)
+        public PLC(int port)
+        {
+            DoTask = Start(port);
+        }
+
+        Task Start(int port)
         {
             return Task.Run(() =>
             {
@@ -58,29 +65,73 @@ namespace MCProtocol
             });
         }
 
-        byte[] ReadRBit(int adr, int len)
+        static byte GetByte(Dictionary<int, bool> dic, int adr)
+        {
+            var b0 = dic.TryGetValue(adr + 0, out bool _b0) && _b0 ? 0x01 : 0x00;
+            var b1 = dic.TryGetValue(adr + 1, out bool _b1) && _b1 ? 0x02 : 0x00;
+            var b2 = dic.TryGetValue(adr + 2, out bool _b2) && _b2 ? 0x04 : 0x00;
+            var b3 = dic.TryGetValue(adr + 3, out bool _b3) && _b3 ? 0x08 : 0x00;
+            var b4 = dic.TryGetValue(adr + 4, out bool _b4) && _b4 ? 0x10 : 0x00;
+            var b5 = dic.TryGetValue(adr + 5, out bool _b5) && _b5 ? 0x20 : 0x00;
+            var b6 = dic.TryGetValue(adr + 6, out bool _b6) && _b6 ? 0x40 : 0x00;
+            var b7 = dic.TryGetValue(adr + 7, out bool _b7) && _b7 ? 0x80 : 0x00;
+            return (byte)(b7 | b6 | b5 | b4 | b3 | b2 | b1 | b0);
+        }
+
+        static byte[] GetWord(Dictionary<int, bool> dic, int adr)
+        {
+            var l = GetByte(dic, adr + 0);
+            var h = GetByte(dic, adr + 8);
+            return new byte[] { l, h };
+        }
+
+        static byte[] GetWord(Dictionary<int, bool> dic, int adr, int len)
         {
             var res = new List<byte>();
-            for (var idx = 0; idx < len; idx += 2)
+            for (var idx = 0; idx < len; idx++)
             {
-                var val0 = (byte)(R.TryGetValue(adr + idx + 0, out bool _val0) && _val0 ? 0x10 : 0);
-                var val1 = (byte)(R.TryGetValue(adr + idx + 1, out bool _val1) && _val1 ? 0x01 : 0);
-                res.Insert(0, (byte)(val0 | val1));
+                res.AddRange(GetWord(dic, adr + idx * 16));
             }
-
             return res.ToArray();
         }
 
-        byte[] ReadMRBit(int adr, int len)
+        static void SetByte(Dictionary<int, bool> dic, int adr, byte val)
         {
-            var res = new List<byte>();
+            dic[adr + 0] = (val & 0x01) != 0;
+            dic[adr + 1] = (val & 0x02) != 0;
+            dic[adr + 2] = (val & 0x04) != 0;
+            dic[adr + 3] = (val & 0x08) != 0;
+            dic[adr + 4] = (val & 0x10) != 0;
+            dic[adr + 5] = (val & 0x20) != 0;
+            dic[adr + 6] = (val & 0x40) != 0;
+            dic[adr + 7] = (val & 0x80) != 0;
+        }
+
+        static byte[] SetWord(Dictionary<int, bool> dic, int adr, int len, byte[] val)
+        {
             for (var idx = 0; idx < len; idx += 2)
             {
-                var val0 = (byte)(MR.TryGetValue(adr + idx + 0, out bool _val0) && _val0 ? 0x10 : 0);
-                var val1 = (byte)(MR.TryGetValue(adr + idx + 1, out bool _val1) && _val1 ? 0x01 : 0);
-                res.Insert(0, (byte)(val0 | val1));
+                SetByte(dic, adr + 0, val[idx * 2 + 0]);
+                if (1 < val.Length)
+                    SetByte(dic, adr + 8, val[idx * 2 + 1]);
             }
+            return Array.Empty<byte>();
+        }
 
+        static byte GetBit(Dictionary<int, bool> dic, int adr)
+        {
+            var val0 = (byte)(dic.TryGetValue(adr + 0, out bool _val0) && _val0 ? 0x10 : 0);
+            var val1 = (byte)(dic.TryGetValue(adr + 1, out bool _val1) && _val1 ? 0x01 : 0);
+            return (byte)(val0 | val1);
+        }
+
+        static byte[] GetBit(Dictionary<int, bool> dic, int adr, int len)
+        {
+            var res = new List<byte>();
+            for (var idx = 0; idx < len; idx++)
+            {
+                res.Insert(0, GetBit(dic, adr + idx * 2));
+            }
             return res.ToArray();
         }
 
@@ -97,25 +148,13 @@ namespace MCProtocol
             return res.ToArray();
         }
 
-        byte[] WriteRBit(int adr, int len, byte[] dat)
+        static byte[] SetBit(Dictionary<int, bool> dic, int adr, int len, byte[] dat)
         {
             for (var idx = 0; idx < len; idx += 2)
             {
-                R[adr + idx + 0] = (dat[idx] >> 4) != 0;
-                R[adr + idx + 1] = (dat[idx] & 0xf) != 0;
+                dic[adr + idx + 0] = (dat[idx] >> 4) != 0;
+                dic[adr + idx + 1] = (dat[idx] & 0xf) != 0;
             }
-
-            return Array.Empty<byte>();
-        }
-
-        byte[] WriteMRBit(int adr, int len, byte[] dat)
-        {
-            for (var idx = 0; idx < len; idx += 2)
-            {
-                MR[adr + idx + 0] = (dat[idx] >> 4) != 0;
-                MR[adr + idx + 1] = (dat[idx] & 0xf) != 0;
-            }
-
             return Array.Empty<byte>();
         }
 
@@ -145,15 +184,19 @@ namespace MCProtocol
             {
                 case 0x0401:
                     //àÍäáì«Ç›çûÇ›
-                    if (dev == 0xa0 && sub == 1) return ReadRBit(adr, len);
-                    if (dev == 0x90 && sub == 1) return ReadMRBit(adr, len);
+                    if (dev == 0xa0 && sub == 0) return GetWord(R, adr, len);
+                    if (dev == 0xa0 && sub == 1) return GetBit(R, adr, len);
+                    if (dev == 0x90 && sub == 0) return GetWord(MR, adr, len);
+                    if (dev == 0x90 && sub == 1) return GetBit(MR, adr, len);
                     if (dev == 0xA8 && sub == 0) return ReadDMWord(adr, len);
                     break;
 
                 case 0x1401:
                     //àÍäáèëÇ´çûÇ›
-                    if (dev == 0xa0 && sub == 1) return WriteRBit(adr, len, dat);
-                    if (dev == 0x90 && sub == 1) return WriteMRBit(adr, len, dat);
+                    if (dev == 0xa0 && sub == 0) return SetWord(R, adr, len, dat);
+                    if (dev == 0xa0 && sub == 1) return SetBit(R, adr, len, dat);
+                    if (dev == 0x90 && sub == 0) return SetWord(MR, adr, len, dat);
+                    if (dev == 0x90 && sub == 1) return SetBit(MR, adr, len, dat);
                     if (dev == 0xA8 && sub == 0) return WriteDMWord(adr, len, dat);
                     break;
 
