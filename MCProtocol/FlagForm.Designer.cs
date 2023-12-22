61c8e5810f7be2b3b1fe02bed0f424aaaea1d0e2
stcsys <ko_nishino@tr-h.co.jp>
2023-12-22 20:29:59 +0900

    fix

---
+++ MCProtocol/FlagForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace MCProtocol
+{
+    partial class FlagForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkedListBox1 = new CheckedListBox();
+            checkedListBox2 = new CheckedListBox();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(12, 31);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(263, 364);
+            checkedListBox1.TabIndex = 0;
+            // 
+            // checkedListBox2
+            // 
+            checkedListBox2.FormattingEnabled = true;
+            checkedListBox2.Location = new Point(281, 31);
+            checkedListBox2.Name = "checkedListBox2";
+            checkedListBox2.Size = new Size(263, 364);
+            checkedListBox2.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(552, 31);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(164, 23);
+            comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Rレジスタ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(281, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 15);
+            label2.TabIndex = 2;
+            label2.Text = "MRレジスタ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(552, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 15);
+            label3.TabIndex = 2;
+            label3.Text = "MRレジスタ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(722, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(722, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(19, 15);
+            label4.TabIndex = 2;
+            label4.Text = "値";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(666, 73);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 51);
+            button1.TabIndex = 4;
+            button1.Text = "読み込み";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(747, 73);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 51);
+            button2.TabIndex = 4;
+            button2.Text = "書き込み";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // FlagForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(864, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(checkedListBox2);
+            Controls.Add(checkedListBox1);
+            Name = "FlagForm";
+            Text = "FlagForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckedListBox checkedListBox1;
+        private CheckedListBox checkedListBox2;
+        private ComboBox comboBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ MCProtocol/FlagForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCProtocol
+{
+    public partial class FlagForm : Form
+    {
+        private int savedScrollPosition = 0;
+        public FlagForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
